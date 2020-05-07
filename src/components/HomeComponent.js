13459f698697984d2fd44a0f9f44c0267723c5e1
Layla <layla.rowen@gmail.com>
2020-05-06 18:59:23 -0700

    #3 added BlogComponent with comments and basic future content

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//will import blog component here
 
 class Home extends Component {
     constructor(props) {
@@ -9,6 +10,7 @@ class Home extends Component {
         return (
             <div>
                 <h1>Home Page Goes Here!</h1>
+                {/*eventually blog will go here*/}
             </div>
         );
     }
