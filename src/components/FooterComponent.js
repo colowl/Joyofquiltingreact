ad8df0bd925ffa634af12f71bc81708917612e1e
colowl <gelatingoodness@gmail.com>
2020-05-15 07:34:06 -0700

    html to React
    I just took the html from the first joyofquilting site and pasted it in the footer component. I deleted the comment because it wasnt in JSX form, though i could of put it in {/**/} for it to work. Lastly i changed all the "class" to "className"

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -7,9 +7,34 @@ class Footer extends Component {
     }
     render() {
         return (
-            <div>
-                <h1>Footer Goes Here!</h1>
+            <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4 col-sm-2">
+                        <h5>Pages</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="/index.html">Home</a></li>
+                            <li><a href="/commissions.html">Commissions</a></li>
+                            <li><a href="/works.html">Previous Works</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-6 col-sm-5 text-center">
+                        <h5>Find Me Here</h5>
+                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i
+                                className="fa fa-instagram"></i></a>
+                        <a className="btn btn-social-icon btn-facebook" href="http://facebook.com/"><i
+                                class="fa fa-facebook"></i></a>
+                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i
+                                class="fa fa-twitter"></i></a>
+                    </div>
+                    
+                    <div className="col-sm-4 col-sm-2 offset-1 text-center">
+                        <a role="button" className="btn btn-link" href="mailto:animusfuro@gmail.com"><i
+                                className="fa fa-envelope-o fa-lg text-primary"></i> animusfuro@gmail.com</a>
+                    </div>
+                </div>
             </div>
+        </footer>
         );
     }
 }
