d06785493a4449e0c9966351607b6d9256967d22
Chris Luong <gelatingoodness@gmail.com>
2020-05-23 13:17:25 -0700

    jumbotron red

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import '../App.css';
 import reactstrap from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.css';
-import { Jumbotron } from 'reactstrap';
-
+import {
+    Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label
+} from 'reactstrap';
 
 class Header extends Component {
     constructor(props) {
@@ -12,33 +15,34 @@ class Header extends Component {
     }
     render() {
         return (
-            <React.Fragment>
-                <Jumbotron fluid>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col center centered-text mx-auto header-title">
-                                <h1>The Joy of Quilting Art Woot</h1>
-                                <h2>by Jamie Rowen</h2>
-                            </div>
+            <div>
+            <Jumbotron className="jelly">
+                <div className="container">
+                    <div className="row">
+                        <div className="col center centered-text mx-auto header-title">
+                            <h1>The Joy of Quilting Art Woot</h1>
+                            <h2>by Jamie Rowen</h2>
+                            <img src={"../img/bigbluequilt.jpg"}/>
+                        </div>
 
-                        </div>
                     </div>
-                </Jumbotron>
+                </div>
+            </Jumbotron>
                 <nav className="navbar navbar-expand-sm navbar-light sticky-top">
-                    <div className="container">
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#joynavbar">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="joynavbar">
-                            <ul className="navbar-nav">
-                                <li className="nav-item active"><a className="nav-link" href="#">Home</a></li>
-                                <li className="nav-item"><a className="nav-link" href="commissions.html">Commissions</a></li>
-                                <li className="nav-item"><a className="nav-link" href="works.html">Previous Works</a></li>
-                            </ul>
-                        </div>
+                <div className="container">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#joynavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="joynavbar">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active"><a className="nav-link" href="#">Home</a></li>
+                            <li className="nav-item"><a className="nav-link" href="commissions.html">Commissions</a></li>
+                            <li className="nav-item"><a className="nav-link" href="works.html">Previous Works</a></li>
+                        </ul>
                     </div>
-                </nav>
-            </React.Fragment>
+                </div>
+            </nav>
+            </div>
         );
     }
 }
