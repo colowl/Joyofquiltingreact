666230b10a34064bcbc6d38c66ab16d436bda86d
Layla <layla.rowen@gmail.com>
2020-05-23 13:33:20 -0700

    blog cards messing around

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,14 +1,52 @@
 import planning from '../shared/images/planning.jpg';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, Label, ModalHeader, ModalBody } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardSubtitle, CardTitle, CardColumns, Button } from 'reactstrap';
 import React from 'react';
 //Do we want this to eventually be a component that pulls from an object of all the blog posts...?
 //This is imported into the HomeComponent
 function Blog(props) {
     return (
         <React.Fragment>
-            <Card>
-                <CardImg src={planning} />
-            </Card>
+            <CardColumns>
+                <Card>
+                    <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+                </Card>
+                <Card>
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+                <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button>Button</Button>
+                </Card>
+                <Card>
+                    <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+                <Card body inverse color="primary">
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button color="secondary">Button</Button>
+                </Card>
+            </CardColumns>
             <div className="container">
                 <div className="row centered-text">
                     <h1 className="blog-title col-sm-12">The Art of Assembling Art</h1>
