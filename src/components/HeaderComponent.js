edfba0daf6f503729826629806590cc98a857726
Chris Luong <gelatingoodness@gmail.com>
2020-05-23 11:41:08 -0700

    class to className and navbar added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,10 +7,11 @@ class Header extends Component {
     }
     render() {
         return (
-            <header className="jumbotron jumbotron-fluid">
-                <div className="container">
-                    <div class="row">
-                        <div className="col center centered-text mx-auto header-title">
+            <div>
+            <header classNameName="jumbotron jumbotron-fluid">
+                <div classNameName="container">
+                    <div classNameName="row">
+                        <div classNameName="col center centered-text mx-auto header-title">
                             <h1>The Joy of Quilting Art Woot</h1>
                             <h2>by Jamie Rowen</h2>
                         </div>
@@ -18,6 +19,21 @@ class Header extends Component {
                     </div>
                 </div>
             </header>
+                <nav className="navbar navbar-expand-sm navbar-light sticky-top">
+                <div className="container">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#joynavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="joynavbar">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active"><a className="nav-link" href="#">Home</a></li>
+                            <li className="nav-item"><a className="nav-link" href="commissions.html">Commissions</a></li>
+                            <li className="nav-item"><a className="nav-link" href="works.html">Previous Works</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            </div>
         );
     }
 }
