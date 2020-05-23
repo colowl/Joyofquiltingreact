20db13fbd82a8b7e9f2b86638ceaf8d84b7a2d65
Layla <layla.rowen@gmail.com>
2020-05-23 13:22:06 -0700

    image variable

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,48 +1,56 @@
-
+import planning from '../shared/images/planning.jpg';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, Label, ModalHeader, ModalBody } from 'reactstrap';
 import React from 'react';
 //Do we want this to eventually be a component that pulls from an object of all the blog posts...?
 //This is imported into the HomeComponent
 function Blog(props) {
     return (
-        <div className="container">
-            <div className="row centered-text">
-                <h1 className="blog-title col-sm-12">The Art of Assembling Art</h1>
-            </div>
-            <div className="row">
-                <div className="col-sm-12 col-md-5">
-                    <img src={"../img/planning.jpg"} className="img-fluid rounded" />
+        <React.Fragment>
+            <Card>
+                <CardImg src={planning} />
+            </Card>
+            <div className="container">
+                <div className="row centered-text">
+                    <h1 className="blog-title col-sm-12">The Art of Assembling Art</h1>
                 </div>
-                <div className="col">
-                    <p className="blog-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.</p>
-                    <a href="https://www.lipsum.com/"><p>Read more...</p></a>
+                <div className="row">
+                    <div className="col-sm-12 col-md-5">
+                        <img src={"../img/planning.jpg"} className="img-fluid rounded" />
+                    </div>
+                    <div className="col">
+                        <p className="blog-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.</p>
+                        <a href="https://www.lipsum.com/"><p>Read more...</p></a>
+                    </div>
                 </div>
-            </div>
-            <div className="row centered-text">
-                <h1 className="blog-title col-sm-12">The Twists and Turns of Quilting</h1>
-            </div>
-            <div className="row">
-                <div className="col-sm-12 col-md-5">
-                    <img src="img/freestyle.jpg" className="img-fluid rounded" />
+                <div className="row centered-text">
+                    <h1 className="blog-title col-sm-12">The Twists and Turns of Quilting</h1>
                 </div>
-                <div className="col">
+                <div className="row">
+                    <div className="col-sm-12 col-md-5">
+                        <img src="img/freestyle.jpg" className="img-fluid rounded" />
+                    </div>
+                    <div className="col">
 
-                    <p className="blog-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.</p>
-                    <a href="https://www.lipsum.com/"><p>Read more...</p></a>
+                        <p className="blog-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.</p>
+                        <a href="https://www.lipsum.com/"><p>Read more...</p></a>
+                    </div>
+                </div>
+                <div className="row centered-text">
+                    <h1 className="blog-title col-sm-12">What To Do With Scraps</h1>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12 col-md-5">
+                        <img src="img/planning.jpg" className="img-fluid rounded" />
+                    </div>
+                    <div className="col">
+                        <p className="blog-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.</p>
+                        <a href="https://www.lipsum.com/"><p>Read more...</p></a>
+                    </div>
                 </div>
             </div>
-            <div className="row centered-text">
-                <h1 className="blog-title col-sm-12">What To Do With Scraps</h1>
-            </div>
-            <div className="row">
-                <div className="col-sm-12 col-md-5">
-                    <img src="img/planning.jpg" className="img-fluid rounded" />
-                </div>
-                <div className="col">
-                    <p className="blog-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.</p>
-                    <a href="https://www.lipsum.com/"><p>Read more...</p></a>
-                </div>
-            </div>
-        </div>
+        </React.Fragment>
+
+
     );
 }
 
---
+++ src/shared/blogs.js
@@ -0,0 +1,19 @@
+export const BLOGS = [
+    {
+        id: 0,
+        name: "The Art of Assembling Art",
+        image: "../img/planning.jpg",
+        featured: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.",
+        link: "https://www.lipsum.com/"
+    },
+    {
+        id: 1,
+        name: "The Art of Assembling Art",
+        image: "../img/planning.jpg",
+        featured: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.",
+        link: "https://www.lipsum.com/"
+
+    }
+];
