a9a4d78927c0106ed8ceaf12f149f21fb7b0ea54
Layla <layla.rowen@gmail.com>
2020-05-23 11:19:25 -0700

    woot header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,17 +7,17 @@ class Header extends Component {
     }
     render() {
         return (
-           <header className="jumbotron jumbotron-fluid">
-            <div className="container">
-                <div class="row">
-                    <div className="col center centered-text mx-auto header-title">
-                        <h1>The Joy of Quilting Art Woo</h1>
-                        <h2>by Jamie Rowen</h2>
+            <header className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <div class="row">
+                        <div className="col center centered-text mx-auto header-title">
+                            <h1>The Joy of Quilting Art Woot</h1>
+                            <h2>by Jamie Rowen</h2>
+                        </div>
+
                     </div>
-                    
                 </div>
-            </div>
-        </header>
+            </header>
         );
     }
 }
