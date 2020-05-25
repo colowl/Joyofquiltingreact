f41339c4511d2314a7ae4023237fd76edf860d93
Chris Luong <gelatingoodness@gmail.com>
2020-05-25 13:32:57 -0700

    added header image

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -23,7 +23,7 @@ class Header extends Component {
                         <div className="col center centered-text mx-auto header-title">
                             <h1>The Joy of Quilting Art Woot</h1>
                             <h2>by Jamie Rowen</h2>
-                            <img src={require('../shared/images/planning.jpg')} />
+                            
                         </div>
 
                     </div>
