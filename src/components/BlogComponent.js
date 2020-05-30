0bae0326274c157a80821a8c1d3e3db444f5aa06
Chris Luong <gelatingoodness@gmail.com>
2020-05-30 13:19:40 -0700

    fontawesome

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -32,7 +32,7 @@ function Blog(props) {
                     </CardBody>
                 </Card>
                 <Card>
-                    <CardImg top width="100%" src={require('../shared/images/squares.jpg')} alt="preparing to cut quilt fabric" />
+                    <CardImg top width="50%" src={require('../shared/images/squares.jpg')} alt="preparing to cut quilt fabric" />
                     <CardBody>
                         <CardTitle><p className="blog-title">What to Do With Scraps</p></CardTitle>
                         <CardText className="blog-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est metus, bibendum vel vulputate placerat, molestie vitae tellus. Sed sodales, tortor vel hendrerit eleifend, tortor est dignissim arcu, id condimentum massa mauris vitae nisl. Vivamus tempor tincidunt eros ac varius. Sed venenatis ex dui, sit amet cursus urna pretium vel. In interdum ipsum eu mi laoreet, vel tempor nunc euismod. Proin lobortis lorem a lectus consectetur vestibulum. Nam volutpat pharetra orci. Sed efficitur a dui vitae consectetur. Ut fermentum consectetur odio, nec dictum leo. Nam ipsum ante, faucibus id sem et, semper cursus metus. Donec lorem tellus, lobortis vitae mollis eget, fermentum quis eros. Phasellus at turpis libero. Nulla aliquet eros tortor, eget tempor eros cursus quis. Proin maximus tellus sit amet neque rhoncus consequat.</CardText>
